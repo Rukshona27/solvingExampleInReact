5c4a984c409e0e38530191d5abb4e310b0e12574
Rukshona27 <rukshona.lu@gmail.com>
2024-02-08 20:33:32 +0300

    btn in React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,23 @@ import './App.css';
 
 import React, { useState } from 'react';
 
-function App(){
-  return
-};
+function App() {
+	const [count, setCount] = useState(0);
+	
+  function plusCount(){
+    setCount(count+1);
+  };
+  function minusCount(){
+    if(count>0){
+      setCount(count-1);
+    }
+  };
+	return <div>
+		 <p>count: {count}</p>
+    <button onClick={()=>{plusCount()}}>+</button>
+    <button onClick={()=>{minusCount()}}>-</button>
+	</div>;
+}
 
 export default App;
 
