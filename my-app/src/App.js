da946d711f540ff5d6219490062deeb6f2689dd9
Rukshona27 <rukshona.lu@gmail.com>
2024-02-08 22:00:45 +0300

    реактивное изменение данных input  и p

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 import React, { useState } from 'react';
 
-function App(){
-  return
-};
+function App() {
+	const [value, setValue] = useState('1');
+  const [value2, setValue2] = useState('2')
+	return <div>
+		<input value={value} onChange={event => (setValue(event.target.value))}/>
+    <p>text: {value}</p>
+    <input value={value2} onChange={event => (setValue2(event.target.value))}/>
+    <p>text2: {value2}</p>
+	</div>;
+}
+
 
 export default App;
 
