70096fc7fe51f7a0eb2ff139fddab20d1c3c1e38
Rukshona27 <rukshona.lu@gmail.com>
2024-02-08 20:31:34 +0300

    main layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,23 +3,9 @@ import './App.css';
 
 import React, { useState } from 'react';
 
-function App() {
-	const [count, setCount] = useState(0);
-	
-  function plusCount(){
-    setCount(count+1);
-  };
-  function minusCount(){
-    if(count>0){
-      setCount(count-1);
-    }
-  };
-	return <div>
-		 <p>count: {count}</p>
-    <button onClick={()=>{plusCount()}}>+</button>
-    <button onClick={()=>{minusCount()}}>-</button>
-	</div>;
-}
+function App(){
+  return
+};
 
 export default App;
 
