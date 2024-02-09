5e07c0573bd7959dc5ece280138dfcf5e28cd377
Rukshona27 <rukshona.lu@gmail.com>
2024-02-09 20:40:51 +0300

    mm

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,26 @@ import './App.css';
 
 import React, { useState } from 'react';
 
-function App(){
-  return
-};
+function App() {
+	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
+  const[value, setValue] = useState('');
+	function addElem(el){
+    let copy = Object.assign([], notes);
+    copy.push(el);
+    setNotes(copy);
+  }
+	const result = notes.map((note, index) => {
+		return <li key={index}>{note}</li>;
+	});
+	
+	return <div>
+		<ul>
+			{result}
+		</ul>
+    <input value={value} onChange={(event)=>{}}/>
+    <button onClick={()=> {addElem(value)}}>add</button>
+	</div>;
+}
 
 export default App;
 
