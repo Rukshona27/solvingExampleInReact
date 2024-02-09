9fc985058a50305cc3b1a0947398e83d7a612b4b
Rukshona27 <rukshona.lu@gmail.com>
2024-02-09 22:11:29 +0300

    Реактивные операции над массивами в React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,22 +5,39 @@ import React, { useState } from 'react';
 
 function App() {
 	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
-  const[value, setValue] = useState('');
-	function addElem(el){
+  const [value, setValue] = useState('');
+	
+	const result = notes.map((note, index) => {
+		return <li key={index} onClick={() => setValue(note, index)}>
+			{note}
+		</li>;
+	});
+	function square(index) {
+		let copy = Object.assign([], notes);
+		copy[index]  = copy[index]*copy[index]; // что-то сделаем с элементом
+		setNotes(copy);
+	}
+  function delEl(index){
     let copy = Object.assign([], notes);
-    copy.push(el);
+    copy.splice(index, 1);
+    setNotes(copy)
+  }
+  function saveInp(val){
+    let copy = Object.assign([], notes);
+    copy[val] = val;
     setNotes(copy);
   }
-	const result = notes.map((note, index) => {
-		return <li key={index}>{note}</li>;
-	});
-	
+  function reverseeArr(){
+    let copy = Object.assign([], notes);
+    copy = copy.reverse();
+    setNotes(copy);
+  }
 	return <div>
 		<ul>
 			{result}
 		</ul>
-    <input value={value} onChange={(event)=>{}}/>
-    <button onClick={()=> {addElem(value)}}>add</button>
+    <input value={value} onChange={(event)=> {setValue(event.target.value)}} onBlur={()=>{saveInp(value)}}/>
+    <button onClick={()=>{reverseeArr()}}>reverse</button>
 	</div>;
 }
 
