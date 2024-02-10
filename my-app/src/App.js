5d0fca7b44c5d0b6693bc309c6142e3eea7ff12c
Rukshona27 <rukshona.lu@gmail.com>
2024-02-10 20:05:37 +0300

    показ описания пи клике

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,57 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  
+  const initNotes = [
+    {
+      id: 'id1',
+      name: 'name1',
+      desc: 'long description 1',
+      show: false,
+    },
+    {
+      id: 'id2',
+      name: 'name2',
+      desc: 'long description 2',
+      show: false,
+    },
+    {
+      id: 'id3',
+      name: 'name3',
+      desc: 'long description 3',
+      show: false,
+    },
+  ];
+  const [notes, setNotes] = useState(initNotes);
 
+  function showDecr(id){
+    setNotes(notes.map(note => {
+      if(note.id === id){
+        note.show = true;
+        return note;
+      }
+      return note;
+    }))
+  }
+  const result = notes.map(note => {
+    if(note.show === false){
+      return <><li key={note.id}>
+			{note.name}</li>
+      <button onClick={()=>{showDecr(note.id)}}>show</button></>
+    }else{
+      return <><li key={note.id}>
+			{note.name}</li>
+      <li>{note.desc}</li>
+      <button onClick={()=>{showDecr(note.id)}}>dont show</button></>
+    };
+	});
+
+   
 	return <div>
-		some
+    <ul>
+      {result}
+    </ul>
 	</div>;
 }
 
