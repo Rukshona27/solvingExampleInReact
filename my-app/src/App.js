66317e1a3fa46366ad1510399fdb13fb31d4beea
Rukshona27 <rukshona.lu@gmail.com>
2024-02-10 13:49:12 +0300

    comm

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,71 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
 import React, { useState } from 'react';
 
 function App() {
-  const initNotes = [
-    {
-      id: 'GYi9G_uC4gBF1e2SixDvu',
-      prop1: 'value11',
-      prop2: 'value12',
-      prop3: 'value13',
-    },
-    {
-      id: 'IWSpfBPSV3SXgRF87uO74',
-      prop1: 'value21',
-      prop2: 'value22',
-      prop3: 'value23',
-    },
-    {
-      id: 'JAmjRlfQT8rLTm5tG2m1L',
-      prop1: 'value31',
-      prop2: 'value32',
-      prop3: 'value33',
-    },
-  ];
-	const [notes, setNotes] = useState(initNotes);
-  const [value, setValue] = useState('');
-  const [value1, setValue1] = useState('');
-  const [value2, setValue2] = useState('');
-	
-	const result = notes.map(note => {
-		return <li key={note.id}>
-			<span>{note.prop1}</span>
-			<span>{note.prop2}</span>
-			<span>{note.prop3}</span>
-		</li>;
-	});
-	function addEl(){
-    const copy = Object.assign([], notes);
-    copy.push({
-      id:"idPush",
-      prop1: 'pushProp1',
-      prop2: 'pushProp2',
-      prop3:'pushProp3',
-    });
-    setNotes(copy);
-  }
-  function addLi(val1, val2,val3){
-    const copy = Object.assign([], notes);
-    copy.push({
-      id: 'idLi',
-      val: {val1},
-      val2: {val2},
-      val3: {val3},
-    });
-    alert(val1, val2, val3)
-    setNotes(copy);
-  }
+  
 
 	return <div>
-		<ul>
-			{result}
-		</ul>
-    <input  value={value} onChange={(event)=>{setValue(event.target.value)}}/>
-    <input  value={value1} onChange={(event)=>{setValue1(event.target.value)}}/>
-    <input  value={value2} onChange={(event)=>{setValue2(event.target.value)}}/>
-    <button onClick={()=>{addLi(value, value1, value2)}}>add</button>
+		some
 	</div>;
 }
 
